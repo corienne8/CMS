bfc6c8800a454a24b20af238a3cf653afc4925f5
corienne8 <u20492422@tuks.co.za>
2024-10-23 11:32:17 +0200

    Homepage code
    Here is the code for the Homepage of our Web Application.
    This includes the tutorial popup and a start button.

---
+++ HomeFunction.js
@@ -0,0 +1,10 @@
+//JavaScript for opening and closing the pop-up
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
